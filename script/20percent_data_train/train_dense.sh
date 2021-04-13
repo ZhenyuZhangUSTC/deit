c8496b9688c5b936cbc1656f91bd38526ddcfb05
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-04-14 00:22:40 +0800

    a

--- script/20percent_data_train/train_dense.sh
+++ script/20percent_data_train/train_dense.sh
@@ -3,7 +3,7 @@ python -m torch.distributed.launch \
     --use_env main.py \
     --model deit_tiny_patch16_224 \
     --batch-size 256 \
-    --data-path /datadrive_c/yucheng/imagenet \
+    --data-path /datadrive_c/tianlong/TLC/imagenet \
     --output_dir experiment/tiny_8gpus_256_dense_20data \
     --data_rate 0.2 \
     --data_split split/ImageNet_train.txt \
