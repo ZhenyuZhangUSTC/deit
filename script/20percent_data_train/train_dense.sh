bc3f9e136cbe0b158bb8f610b18b275b13764d7f
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-04-13 23:52:42 +0800

    a

--- script/20percent_data_train/train_dense.sh
+++ script/20percent_data_train/train_dense.sh
@@ -7,4 +7,5 @@ python -m torch.distributed.launch \
     --output_dir experiment/tiny_8gpus_256_dense_20data \
     --data_rate 0.2 \
     --data_split split/ImageNet_train.txt \
-    --init_weight deit_tiny_mask_init/random_init.pt 
+    --init_weight deit_tiny_mask_init/random_init.pt \
+    --dist_url 'tcp://127.0.0.1:33067'
