f0787c11e439a714812614aed2c92fb2c8e49605
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-04-14 00:14:07 +0800

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -15,7 +15,6 @@ from torch.utils.data import Dataset
 class split_imagenet_dataset(Dataset):
 
     def __init__(self, root, txt, rate=1., transform=None):
-        print(root)
         self.img_path = []
         self.labels = []
         self.root = root
