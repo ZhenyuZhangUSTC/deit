4fb4a458ec8507b5808a5345e4b874837a589da3
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-04-15 20:08:26 +0800

    result

--- engine.py
+++ engine.py
@@ -63,6 +63,90 @@ def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
 
+# To Be delete
+def train_one_epoch_test(model: torch.nn.Module, criterion: DistillationLoss,
+                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
+                    device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
+                    model_ema: Optional[ModelEma] = None, mixup_fn: Optional[Mixup] = None,
+                    set_training_mode=True):
+    model.train(set_training_mode)
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 10
+
+    scaler = torch.cuda.amp.GradScaler()
+
+    for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
+        samples = samples.to(device, non_blocking=True)
+        targets = targets.to(device, non_blocking=True)
+
+        if mixup_fn is not None:
+            samples, targets = mixup_fn(samples, targets)
+
+        with torch.cuda.amp.autocast():
+            outputs = model(samples)
+            loss = criterion(samples, outputs, targets)
+
+        loss_value = loss.item()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)
+
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+
+
+        # show gradient sparsity 
+        grad_zero = 0
+        elements = 0
+
+        for name, p in model.named_parameters():
+            if p.requires_grad:
+                if not p.grad == None:
+                    gzero = (p.grad == 0).float().sum().item()
+                    ele = p.grad.nelement()
+                    grad_zero += grad_zero
+                    elements += ele 
+                    print(name, 'sparsity = {:.2f}%'.format(100*gzero/ele))
+                else:
+                    print(name, 'grad = None')
+
+        print('total gradient sparsity = {:.2f}%, all-element = {}'.format(100*grad_zero/elements, elements))
+
+        # calculate weight sparsity 
+        weight_zero = 0
+        elements = 0
+        for key in model.state_dict().keys():
+            if 'mask' in key:
+                print(key)
+                weight_zero += (model.state_dict()[key] == 0).float().sum().item()
+                elements += model.state_dict()[key].nelement()
+        print('weight sparsity = {:.2f}%'.format(100*weight_zero/elements))
+
+        assert False 
+
+
+        # this attribute is added by timm on one optimizer (adahessian)
+        is_second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
+        loss_scaler(loss, optimizer, clip_grad=max_norm,
+                    parameters=model.parameters(), create_graph=is_second_order)
+
+        torch.cuda.synchronize()
+        if model_ema is not None:
+            model_ema.update(model)
+
+        metric_logger.update(loss=loss_value)
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
+
+
 @torch.no_grad()
 def evaluate(data_loader, model, device):
     criterion = torch.nn.CrossEntropyLoss()
