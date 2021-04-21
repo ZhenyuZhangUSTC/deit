ce09a7241e663cd1cc3455ea727637fc6a85e7f7
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-04-21 21:08:09 +0800

    upload

---
+++ script/omp_base/train_base_omp_node1.sh
@@ -0,0 +1,15 @@
+python -m torch.distributed.launch \
+    --nproc_per_node=4 \
+    --nnodes=3 \
+    --node_rank=1 \
+    --master_addr='yucheng@10.124.136.83' \
+    --master_port=30445 \
+    --use_env main.py \
+    --model deit_base_patch16_224 \
+    --batch-size 256 \
+    --data-path /datadrive_d/tlc/imagenet \
+    --init_mask deit_mask/deit_base_patch16_224_omp50_mag.pt \
+    --output_dir experiment/deit_base_omp_50
+
+
+
